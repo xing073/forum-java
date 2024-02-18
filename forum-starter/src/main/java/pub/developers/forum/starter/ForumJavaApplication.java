f9e5683304f0b925fb40f1fb8206a041bf62cc1c
xing <2867060549@qq.com>
2024-02-18 22:22:04 +0800

    master second commit

--- forum-starter/src/main/java/pub/developers/forum/starter/ForumJavaApplication.java
+++ forum-starter/src/main/java/pub/developers/forum/starter/ForumJavaApplication.java
@@ -14,7 +14,7 @@ public class ForumJavaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumJavaApplication.class, args);
-		System.out.println("git test");
+		System.out.println("master -- git test");
 	}
 
 	@Override
